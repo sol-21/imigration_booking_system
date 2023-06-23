093e2a5c94e1c8fc0b5e7c501376951f750cffa3
Getasew21 <getasewadane9@gmail.com>
2023-06-23 14:27:17 +0300

    react bootstrap packed installed and topnavigation ,footer ,custom_css created and applogo changed

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,12 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../assets/css/custom.css";
+
+import TopNavigation from './Components/TopNavigation';
+
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -13,7 +19,7 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(<TopNavigation />);
     },
     progress: {
         color: '#4B5563',
