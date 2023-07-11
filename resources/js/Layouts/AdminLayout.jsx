686c65d3a9725a876a1be274943ce6ac05b0c14a
Getasew21 <getasewadane9@gmail.com>
2023-07-11 09:45:27 +0300

    some correction

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,24 +1,21 @@
+import {
+    Menu,
+    MenuItem,
+    Sidebar,
+    SubMenu,
+    sidebarClasses,
+} from "react-pro-sidebar";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import TopNavAdmin from "@/Components/TopNavAdmin";
+import SideBar from "@/Components/SideBar";
 
-
-
-import { Menu, MenuItem, Sidebar, SubMenu, sidebarClasses } from 'react-pro-sidebar';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import TopNavAdmin from '@/Components/TopNavAdmin';
-import SideBar from '@/Components/SideBar';
-
-
-function AdminLayout() {
-
-
-
-
+function AdminLayout({ auth, Children }) {
     return (
         <div className="min-h-screen ">
-            <TopNavAdmin />
+            <TopNavAdmin auth={auth} />
+            <main>{Children}</main>
             <SideBar />
-
-
         </div>
     );
 }
