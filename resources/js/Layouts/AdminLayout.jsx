bc229a429e4f65b6fe789f49c8efefeb514abe3f
Getasew21 <getasewadane9@gmail.com>
2023-07-17 10:20:02 +0300

    cleaning

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,12 +1,3 @@
-import {
-    Menu,
-    MenuItem,
-    Sidebar,
-    SubMenu,
-    sidebarClasses,
-} from "react-pro-sidebar";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TopNavAdmin from "@/Components/TopNavAdmin";
 import SideBar from "@/Components/SideBar";
 
