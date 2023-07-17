31ffe45a3e422ba85e3eaa106dd22e0dacb78137
Getasew21 <getasewadane9@gmail.com>
2023-07-17 14:36:55 +0300

    admin contrller created

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointement;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users= User::all();
+        $appointements= Appointement::all();
+        return Inertia::render('Admin/HomePage',['users'=>$users,'appointements'=>$appointements]);
+    }
+
+    
+}
